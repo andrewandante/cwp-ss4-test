e3844475944216eea61566a64cf6950910cccb32
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-09-27 10:55:23 +0100

    initial commit beta3

---
+++ mysite/code/HomePage.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Andante\Pages;
+
+class BiodomePage extends \Page {
+
+    private static $icon = 'dopekeen.png';
+}
---
+++ mysite/code/HomePageController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Andante\Pages;
+
+class BiodomePageController extends \PageController
+{
+
+}
